e28ab9eeb30193dfcbd6502452d6339f5cbe1cff
bubupu <3010674713@qq.com>
2020-01-09 15:52:46 +0800

    aaa

--- src/component/cart/index.jsx
+++ src/component/cart/index.jsx
@@ -8,7 +8,11 @@ export default class extends React.Component {
 			<div className={className}>
 				<h1>{JSON.parse(data.info).Door}</h1>
 				<p>{JSON.parse(data.info).homesize}</p>
-				<img src={JSON.parse(data.info).updatetime} alt="" />
+				<img
+					src={Array.isArray(JSON.parse(data.info).updatetime)
+						? JSON.parse(data.info).updatetime[0]
+						: JSON.parse(data.info).updatetime} alt=""
+				/>
 				<p>{JSON.parse(data.info).address}</p>
 				<p>{JSON.parse(data.info).homeone}</p>
 				<p className="mess">
